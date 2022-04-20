6152d92dac8b7e91e1ab44cf14dc1fdbaaf8eb2c
Baumel Márton <marcibaumel@gmail.com>
2022-04-20 11:06:55 +0200

    film models

---
+++ Assigment/NextFilm/DataAccess/Models/FilmModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess.Models
+{
+    [Table("FilmTable")]
+    public class FilmModel : BaseModel
+    {
+        public string Title { get; set; }
+        public string Plot { get; set; }
+        public DateTime RealeaseDate { get; set; }
+        public string Actors { get; set; }
+        public string Poster { get; set; }
+        public Boolean IsActive { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public FilmModel(string title, string plot, DateTime realeaseDate, string actors, string poster, bool isActive, DateTimeOffset createdDate)
+        {
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Plot = plot ?? throw new ArgumentNullException(nameof(plot));
+            RealeaseDate = realeaseDate;
+            Actors = actors ?? throw new ArgumentNullException(nameof(actors));
+            Poster = poster ?? throw new ArgumentNullException(nameof(poster));
+            IsActive = isActive;
+            CreatedDate = createdDate;
+        }
+    }
+}
