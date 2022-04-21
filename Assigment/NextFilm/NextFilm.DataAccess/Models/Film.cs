c57d2a9ab41b5844756ccf84166cbe0db70ae308
Baumel Márton <marcibaumel@gmail.com>
2022-04-21 19:43:45 +0200

    pain

---
+++ Assigment/NextFilm/NextFilm.DataAccess/Models/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextFilm.DataAccess.Models
+{
+    public class Film
+    {
+        public int FilmId { get; set; }
+        public string Title { get; set; }
+        public string Poster { get; set; }
+        public bool IsWatched { get; set; }
+        public DateTime ReleaseYear { get; set; }
+        public DateTimeOffset AddedDate { get; set; }
+        public User User { get; set; }
+    }
+}
